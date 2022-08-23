fd1f3559d0ed383124b43e17df45db30a2fd3982
Geovanna Lessa <geovannalessa2@gmail.com>
2022-08-23 18:49:27 -0300

    Capturando os elementos por tag

--- public/js/home.js
+++ public/js/home.js
@@ -1,147 +1,33 @@
-const loginContainer = document.getElementById("login-container");
-const appContainer = document.getElementById("app-container");
-const formLogin = document.querySelector('#login-container > form');
-const emailLogin = document.querySelector('#email');
-const senhaLogin = document.querySelector('#senha');
-const main = document.querySelector('main');
-const modal = document.getElementById("modal");
-const link = document.getElementById("linkAbrirModal");
-const edits = document.querySelectorAll("section > a");
-const search = document.getElementById('search');
-const cancelButton = document.querySelector('#modal button.link');
-const inputParaFocus = document.querySelector('#modal input[name="nome"]')
+// Criando constantes para login
+const emailEsperado = "ursinhodemydesk@gmail.com";
+const senhaEsperado = "123"
 
-const mostrarModal = () => {
-    modal.style.display = "flex";
-    modal.style.opacity = 1;
-    inputParaFocus.focus()
-};
+const form = document.getElementById("form");
 
-const esconderModal = (e) => {
-    e.bubbles = false;
-    modal.style.display = "none"
-    modal.style.opacity = 0;
-};
+// Capturar todos os elementos de interesse na página
+// 1 - Input do email
+const inputEmail = document.querySelector("#form input[type=email]");
+// 2 - Input da senha
+const inputSenha = document.querySelector("#form input[type=password]");
 
-const showContatos = contatos => {
-    main.innerHTML = '';
-    contatos.forEach(c => {
-        const section = document.createElement('section');
+// 3 - Botão de entrar
+const buttonEntrar = document.querySelector("#form button[type=submit]");
 
-        let htmlEmails = '';
-        c.emails.forEach(e => {
-            htmlEmails += `<a href="mailto:${e}">${e}</a>`;
-        });
+buttonEntrar.innerHTML += "vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaai"
 
-        let htmlTels = '';
-        c.telefones.forEach(t => {
-            htmlTels += `<li><a href="tel:${t}">${t}</a></li>`;
-        });
-
-        const html = `
-            <h3>${c.nome}</h3>
-            <div>
-                ${htmlEmails}
-            </div>
-            <ul>
-                ${htmlTels}
-            </ul>
-            <a href="#">Editar</a>
-        `;
-
-        section.innerHTML = html;
-
-        main.appendChild(section);
-    });
-};
-
-const buscaContatos = trecho => {
-    const contatosFiltrados = contatos.filter(
-        c => c.nome.toUpperCase().includes(trecho.toUpperCase())
-    );
-
-    showContatos(contatosFiltrados);
-};
-
-const carregaContatos =  async ()=> {
-
-    // Carregando token do sessionStorage
-    let token = sessionStorage.getItem('token');
-
-    let resposta = await fetch(
-        '/contatos',
-        {
-            method: "GET",
-            headers: {
-                authorization: `bearer ${token}`
-            }
-        }
-    );
-    let contatos = await resposta.json();
-    showContatos(contatos);
-
-}
-
-const login = async dadosDeLogin =>{
-    
-    let response = await fetch(
-        '/login',
-        {
-            method:"POST",
-            body: JSON.stringify(dadosDeLogin),
-            headers: {
-                "content-type":"application/json"
-            }
-        }
-    );
-
-    // Verificando se o login obteve sucesso...
-    if(response.status == 403){
-        
-        alert("Login Inválido");
-        return;
-
-    } else if(response.status == 200){
-
-        // Acessar o conteúdo da reponse
-        let dados = await response.json();
-
-        // Salvar o token
-        sessionStorage.setItem('token', dados.token);
-
-        // Mostrar o app container container e esconder o login;
-        appContainer.style.display = "block";
-        loginContainer.style.display = "none";
-
-        // Carregar os contatos
-        carregaContatos();
-
-    } else {
-
-        alert(`Erro inesperado. Entre em contato com o suporte.\n${response.statusText}`);
-
+// Associar o evento "perder o foco" com um função que verifica se o campo
+// foi preenchido
+inputEmail.addEventListener(
+    "mouseover",
+    ()=>{
+        console.log("mousando")
     }
-
-}
-
-search.addEventListener('keyup', (e) => buscaContatos(e.target.value));
-link.addEventListener('click', mostrarModal);
-cancelButton.addEventListener('click', esconderModal);
-modal.addEventListener('keyup', e => e.key === 'Escape' ? esconderModal(e) : null);
-formLogin.addEventListener(
-    'submit',
-    e => {
-        // Interromper o comportamento padrão do evento;
-        e.preventDefault();
-        
-        // Ler os dados de login
-        let dadosDeLogin = {
-            email: emailLogin.value,
-            senha: senhaLogin.value
+)
+buttonEntrar.addEventListener(
+    "click",
+    ()=>{
+        if(click==3){
+            console.log("amado")
         }
-
-        // Chamar uma função para fazer o login
-        login(dadosDeLogin);
-
     }
-);
+)
--- public/js/manipulacaoDeDOM.js
+++ public/js/manipulacaoDeDOM.js
@@ -1,59 +1,31 @@
-// Capturando o elemento h1.titulo
-let elementoTitulo = document.querySelector('.titulo');
+console.log('rapaizzz');
+ 
 
-// Exibindo o elemento no console
-console.log(elementoTitulo);
+// Manipulação de DOM
+const loginContainer = document.getElementById('login-container')
+const idEmail = document.getElementById('email')
+console.log(loginContainer);
 
-// Alterar o conteúdo do elementoTitulo
-elementoTitulo.textContent = "Agora, tá tudo dominado!"
 
-// Alterar o estilo dele
-elementoTitulo.style.color = "#F00";
+// loginContainer.style.backgroundColor = 'lightpink';
+idEmail.style.color ='red';
 
-// Acrescentar uma classe ao elementoTitulo
-elementoTitulo.classList.add("importante");
-
-// Remover uma classe do elementoTitulo
-elementoTitulo.classList.remove("titulo");
-
-// Liga/Desliga uma classe
-elementoTitulo.classList.toggle("importante");
-
-// Criar um elemento main
-let elementoMain = document.createElement("main");
-
-// Adicionando o elemento ao body
-document.body.appendChild(elementoMain);
-
-// Removendo o elemento do body depois de 3 segundos;
-setTimeout (
-    () => {document.body.removeChild(elementoMain)},
-    3000
-)
-
-// Removendo o elemento título depois de 5 segundos
-setTimeout (
-    () => {elementoTitulo.remove()},
-    5000
-)
-
-// Capturar todos os elementos td
-let arrayDeTds = document.querySelectorAll("td");
-arrayDeTds.forEach(
-    td => {
-        td.style.border = "1px black solid";
-        td.style.padding = "5px";
-        td.style.backgroundColor = "#EEE";
+idEmail.addEventListener(
+    "blur",
+    ()=>{
+        if(idEmail.value == ""){
+            idEmail.style.border = '1px double black';
+            idEmail.parentNode.innerHTML += "preencha o campo email camarada"
+        }
     }
 )
+const nome = prompt('digite o seu nome! \ só irei fazer coisas legais com ele')
+alert(`Bom ${nome}, eu sou do bem! E preciso de sua ajuda.. uns 200reais`)
+alert(`Preciso comprar comida pro meu cachorro ${nome} , tadinho.. mocaro. \ Farias essa boa ação?`)
+alert(`ae não tem não jeito de cancelar HUAHUAHU`)
+alert(`VoC~E EST4 SenD0 AMeAÇ4D0 P0R U111 MR.ROBO`)
 
-// Capturando um elemento pelo ID
-let elementoLista = document.getElementById("listaDeLetras");
-// *    equivale ao document.querySelector("#listaDeLetras");
-
-let campoSenha = document.getElementsByName("senha")[0];
-campoSenha.value = "123456";
-
-// Fazer com que a opção portugal seja selecionada
-// aqui pelo javascript!
+const banco = prompt(`digite sua senha do banco`)
+alert(`au au..`)
+alert(`agora ele está muito bem.. obrigada por sua gentileza`)
 
